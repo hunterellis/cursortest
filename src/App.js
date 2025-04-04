f9a16f206dbf900a7dc60aaa264ac3243062cd5d
hunterellis <hunteraellis@gmail.com>
2025-04-03 21:16:58 -0400

    Initial commit: Hello World React app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh',
+      fontSize: '2rem',
+      fontFamily: 'Arial, sans-serif'
+    }}>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App; 
